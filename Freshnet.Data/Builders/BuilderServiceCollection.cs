869d7d1a430b053098921062f20c92bf8aa5302a
Maikel Veen <veenmaikel@gmail.com>
2020-04-20 17:10:53 +0200

    FC-17 Add Builder Service Collection

---
+++ Freshnet.Data/Builders/BuilderServiceCollection.cs
@@ -0,0 +1,10 @@
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class BuilderServiceCollection
+    {
+        public static IServiceCollection AddBuilderServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
