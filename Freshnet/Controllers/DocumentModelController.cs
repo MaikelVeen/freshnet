882deeb4054399b33c5e0bc906605b133c5d03af
Maikel Veen <veenmaikel@gmail.com>
2020-04-20 18:25:46 +0200

    FC-7 Implement IMaintable in Document Controller

--- Freshnet/Controllers/DocumentModelController.cs
+++ Freshnet/Controllers/DocumentModelController.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using Freshnet.Data.DataTransferObjects;
 using Freshnet.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace Freshnet.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class DocumentModelController : ControllerBase
+    [Route("[controller]/[action]")]
+    public class DocumentModelController : ControllerBase, IMaintainable<DocumentModel, DocumentModelDto>
     {
         [HttpPost]
-        public IActionResult Create(DocumentModel documentModel)
+        public ActionResult<DocumentModel> Create([FromBody]DocumentModelDto obj)
+        {
+            DocumentModel documentModel = new DocumentModel();
+            documentModel.Name = "Test";
+            return documentModel;
+        }
+        
+        [HttpGet]
+        public ActionResult<DocumentModel> Retrieve(ObjectId key)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPut]
+        public IActionResult Update(DocumentModelDto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(ObjectId key)
         {
             throw new NotImplementedException();
         }
