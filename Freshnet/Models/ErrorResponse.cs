6db8fa33ba64dc29c17f5fe3b5d99621bcf71b49
Maikel Veen <veenmaikel@gmail.com>
2020-04-23 22:47:57 +0200

    FC-7 Add ErrorResponse class

---
+++ Freshnet/Models/ErrorResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Freshnet.Models
+{
+    public class ErrorResponse 
+    {
+        private List<Error> Errors { get; set; }
+
+        public void AppendError(Exception exception, int status)
+        {
+            Errors.Add(new Error(exception.Message, status));
+        }
+        
+        public void AppendError(string message, int status)
+        {
+            Errors.Add(new Error(message, status));
+        }
+    }
+
+    public class Error
+    {
+        public Error(string message, int status)
+        {
+            Message = message;
+            Timestamp = DateTime.Now;
+            Status = status;
+        }
+
+        private string Message { get; set; }
+        private int Status { get; set; }
+        private DateTime Timestamp { get; set; }
+    }
+}
